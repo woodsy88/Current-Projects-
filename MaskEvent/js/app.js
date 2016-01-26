6ad4bd2e9d02755af52dee4cfba273b519ad2b01
woodsy88 <andrewwoods88@gmail.com>
2016-01-25 21:29:56 -0500

    Almost complete mask project
    updated icons
    added animations
    added links

--- MaskEvent/js/app.js
+++ MaskEvent/js/app.js
@@ -1,46 +1,8 @@
-var myApp = angular.module('myApp', ['ngRoute']);
-
-myApp.config(function($routeProvider){
-    
-    $routeProvider
-    
-    .when('/',{
-        templateUrl: 'pages/clue1.html',
-        controller: 'mainController'
-        
-    })
-    
-    
-    .when('/second',{
-        templateUrl: 'pages/clue2.html',
-        controller:'secondController'        
-        
-    })
-    
-    .when('/third',{
-        
-        templateUrl: 'pages/clue3.html',
-        controller: 'thirdController'
-    })
-});
-
+var myApp = angular.module('myApp', []);
 
 
 myApp.controller('mainController', ['$scope', function($scope) {
     
 $scope.name = "Main";
-    
-}]);
-
-
-myApp.controller('secondController', ['$scope', '$log', function($scope, $log){
-    
-    $scope.name = 'second';
-    
-}]);
-
-myApp.controller('thirdController', ['$scope', '$log', function($scope, $log){
-    
-    $scope.name = 'third';
     
 }]);
